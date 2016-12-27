ae633e356ded49407250740ba77ee5ef445b3600
Dima Knivets <knivets@gmail.com>
2016-12-27 18:45:34 -0500

    .

--- markparser/storage.py
+++ markparser/storage.py
@@ -1,23 +1,19 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import os
+import sys
+import pdb
+sys.path.append(os.path.abspath('../mark-admin'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-Base = declarative_base()
- 
-class Place(Base):
-  __tablename__ = 'places'
-  id = Column(Integer, primary_key=True)
-  title_ru = Column(String(500), nullable=False)
-  title_ua = Column(String(500), nullable=False)
-  phone = Column(String(500))
-  address = Column(String(150))
-  website = Column(String(150))
+import django
+django.setup()
 
-engine = create_engine('sqlite:///store.db') 
-Base.metadata.create_all(engine)
+from places.models import Place
+from django.db.models import Q
 
-def get_session():
-  Base.metadata.bind = engine
-  return sessionmaker(bind=engine)()
+def get_places(q):
+	places = Place.objects.filter(Q(title_ru__icontains=q) | Q(title_ua__icontains=q))
+	if places:
+		return places[0]
+
+def create_place(place):
+	return Place(**place).save()
