f90b27685150c635fafe240fade21f4ac2f90372
Dima Knivets <knivets@gmail.com>
2016-12-31 17:17:28 +0200

    websites and phone formatting

--- markparser/spiders/mapvn.py
+++ markparser/spiders/mapvn.py
@@ -16,7 +16,7 @@ def extract_title_and_kind(string):
   kind = None
   # removing whitespace from the right otherwise our match might fail
   # then reversing the string to facilitate right to left match
-  reverse = string.rstrip()[::-1]
+  reverse = string.strip()[::-1]
   result = matcher.match(reverse)
   if result:
     # reversing back, stripping space and parenthesis
@@ -43,6 +43,27 @@ def extract_city_and_address(string):
   address = (','.join(coms[1:])).strip()
   return (address, city)
 
+def website_validator(website):
+  return website and website.strip() and ('.' in website) and ('map.vn.ua' not in website)
+
+def decode_and_format_phone(string):
+  phone_s = base64.b64decode(string).decode('utf-8')
+  phone_s = phone_s[3:] # remove 'т. ' 
+  # removing whitespace and dashes
+  phone_s = phone_s.replace(' ', '').replace('-', '').replace('&ndash;', '')
+  phones = phone_s.split(',')
+  fin_phones = []
+  for p in phones:
+    if p:
+      norm_p = p
+      if norm_p[0] != '+':
+        # adding phone code
+        norm_p = '+38' + norm_p
+      if ')(' in norm_p: # removing duplicate area code
+        norm_p = norm_p.replace('(0432)(0432)', '(0432)')
+      fin_phones.append(norm_p)
+  return ', '.join(fin_phones)
+
 class MapvnSpider(scrapy.Spider):
     name = "mapvn"
     allowed_domains = ["map.vn.ua"]
@@ -59,22 +80,25 @@ class MapvnSpider(scrapy.Spider):
         item_sel = Selector(text=item)
         record = PlaceItem()
         title = item_sel.css('a.catHeader::text').extract_first()
+        address = item_sel.css('div.infoBlock::text').extract_first()
+        website = item_sel.css('div.buSite a::text').extract_first()
+        phone = item_sel.css('div.phoneFade::attr(phone)').extract_first()
         if title:
+          title = unicodedata.normalize('NFKD', title)
           record['title'], kind = extract_title_and_kind(title)
           if kind:
             record['kind'] = kind
-        address = item_sel.css('div.infoBlock::text').extract_first()
-        if address.strip():
+        if address and address.strip():
+          address = unicodedata.normalize('NFKD', address)
           record['address'], city = extract_city_and_address(address)
           if city:
             record['city'] = city
-        record['website'] = item_sel.css('div.buSite a::text').extract_first()
-        phone = item_sel.css('div.phoneFade::attr(phone)').extract_first()
+        if website_validator(website):
+          website = unicodedata.normalize('NFKD', website)
+          record['website'] = website.strip()
         if phone:
-          record['phone'] = base64.b64decode(phone).decode('utf-8')
-        for key, val in record.items():
-          if val:
-            record[key] = unicodedata.normalize('NFKD', val)
+          phone = unicodedata.normalize('NFKD', phone)
+          record['phone'] = decode_and_format_phone(phone)
         yield record
       pages = response.css('div.companiesListPager a.pager-page::attr(href)').extract()
       for page in pages:
